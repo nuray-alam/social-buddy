808df6b6e51f90d53f942347d73fc510b4233951
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-03-28 19:58:18 +0600

    dependencies added

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -12,13 +12,13 @@ const PostDetail = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setPost(data))
-    }, [])
+    }, [id])
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setComments(data))
-    }, [])
+    }, [id])
     return (
         <div>
             <h1>This is Post Detail id: {id}</h1>
